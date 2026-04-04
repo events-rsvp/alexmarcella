d16fa04d01f793b656165a50dfc60d4dd0367a3a
livingbeings <livingbeings@users.noreply.github.com>
2026-04-04 07:03:40 +0000

    deploy: livingbeings/wedding-rsvp@4b2918f8ddafeb0ec8873cf4321bfa3174788f44

--- js/scripts.js
+++ js/scripts.js
@@ -151,23 +151,10 @@ $(document).ready(function () {
             id: ''
         },
         data: {
-            // Event title
             title: "Marcella and Alex's Wedding",
-
-            // Event start date
-            start: new Date('June 6, 2026 10:00'),
-
-            // Event duration (IN MINUTES)
-            // duration: 120,
-
-            // You can also choose to set an end time
-            // If an end time is set, this will take precedence over duration
-            end: new Date('June 6, 2026 00:00'),
-
-            // Event Address
+            start: new Date('June 6, 2026 09:00'),
+            end: new Date('June 6, 2026 20:00'),
             address: 'Padma Ubud',
-
-            // Event Description
             description: "We can't wait to see you on our big day. For any queries or issues, please contact us."
         }
     });
